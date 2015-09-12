e14be05aa05b290a91b7599c695a0e6c72452f1a
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2015-09-12 01:07:59 +0000

    Add used functions to extconf.rb

--- ext/tox/extconf.rb
+++ ext/tox/extconf.rb
@@ -6,5 +6,8 @@ NAME = 'tox'
 
 LIBTOXCORE = 'toxcore'
 have_library LIBTOXCORE, 'tox_options_default'
+have_library LIBTOXCORE, 'tox_new'
+have_library LIBTOXCORE, 'tox_get_savedata_size'
+have_library LIBTOXCORE, 'tox_get_savedata'
 
 create_makefile "#{NAME}/#{NAME}"
