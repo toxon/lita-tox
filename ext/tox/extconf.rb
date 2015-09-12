c19fd24c5e6f704a8538b9d8a44178334da75de1
Braiden Vasco <braiden-vasco@users.noreply.github.com>
2015-09-12 17:35:42 +0000

    Make real checks in extconf.rb

--- ext/tox/extconf.rb
+++ ext/tox/extconf.rb
@@ -5,11 +5,12 @@ require 'mkmf'
 NAME = 'tox'
 
 LIBTOXCORE = 'toxcore'
-have_library LIBTOXCORE, 'tox_options_default'
-have_library LIBTOXCORE, 'tox_new'
-have_library LIBTOXCORE, 'tox_get_savedata_size'
-have_library LIBTOXCORE, 'tox_get_savedata'
-have_library LIBTOXCORE, 'tox_self_get_address'
-have_library LIBTOXCORE, 'tox_bootstrap'
 
-create_makefile "#{NAME}/#{NAME}"
+have_library LIBTOXCORE, 'tox_options_default' and
+have_library LIBTOXCORE, 'tox_new' and
+have_library LIBTOXCORE, 'tox_get_savedata_size' and
+have_library LIBTOXCORE, 'tox_get_savedata' and
+have_library LIBTOXCORE, 'tox_self_get_address' and
+have_library LIBTOXCORE, 'tox_bootstrap' and
+
+create_makefile "#{NAME}/#{NAME}" or exit(1)
